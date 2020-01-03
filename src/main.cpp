3299af24d2db1aa0dc99addc1d7a9d922af73ad4
ggerardy <liftchampion@gmail.com>
2020-01-03 22:37:44 +0300

    Vulkan window is opening
    Took 5 minutes

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #define GLFW_INCLUDE_VULKAN
+
 #include <GLFW/glfw3.h>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
 #include <iostream>
 
-int main() {
+int main()
+{
   glfwInit();
 
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+  GLFWwindow *window = glfwCreateWindow(800, 600, "Vulkan window", nullptr,
+										nullptr);
 
   uint32_t extensionCount = 0;
   vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
@@ -21,9 +25,9 @@ int main() {
 
   glm::mat4 matrix;
   glm::vec4 vec;
-  auto test = matrix * vec;
+  auto      test          = matrix * vec;
 
-  while(!glfwWindowShouldClose(window)) {
+  while (!glfwWindowShouldClose(window)) {
 	glfwPollEvents();
   }
 
